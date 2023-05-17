c7616a38f6d4d43efea8ffaa8ef5c19092ef4ab8
Omar <omar.din@youlend.com>
2023-05-17 13:46:21 +0100

    feat: master: update code to use azuredevops rather than git

--- set_git_details.sh
+++ set_git_details.sh
@@ -1,20 +1,21 @@
 echo "#=================================#"
 echo "# Please enter Git details now    #"
 echo "#=================================#"
-read -p 'Git Username: ' GIT_USER
-read -p 'Git Token: ' GITHUB_TOKEN
-read -p 'Git Repo to be Created (eg. keptndemo): ' GIT_NEW_REPO_NAME
 
-export GIT_REPO=https://github.com/$GIT_USER/$GIT_NEW_REPO_NAME.git
-export GIT_USER=$GIT_USER
-export GITHUB_TOKEN=$GITHUB_TOKEN
-export GIT_NEW_REPO_NAME=$GIT_NEW_REPO_NAME
+read -p 'Azure DevOps User: ' AZDO_USER
+read -p 'Azure DevOps Token: ' AZDO_TOKEN
+read -p 'Azure DevOps Repo name: ' AZDO_REPO_NAME
+
+
+export AZDO_USER=$AZDO_USER
+export AZDO_REPO_NAME=$AZDO_REPO_NAME
+export AZDO_REPO_URL= https://youlend@dev.azure.com/youlend/Youlend-Infrastructure/_git/$AZDO_REPO_NAME
+export AZDO_TOKEN=$AZDO_TOKEN
 
 echo ""
 echo "#=================================#"
 echo "         Git Details Set:          "
 echo "#=================================#"
 echo ""
-echo "Git Username: $GIT_USER"
-echo "Git Token: $GITHUB_TOKEN"
-echo "New Git repo to be created: $GIT_REPO"
+echo "Azure DevOps Token: $AZDO_TOKEN"
+echo "Azure DevOps repo: $AZDO_REPO_URL"
--- setup_keptn.sh
+++ setup_keptn.sh
@@ -1,14 +1,25 @@
 cd
 echo ""
-echo "================================================================================="
-echo " Creating brand new Git repo ($GIT_NEW_REPO_NAME) on your GitHub.com account     "
-echo "================================================================================="
-gh repo create $GIT_NEW_REPO_NAME --public
-
-echo ""
 echo ""
 echo "================================================================================="
 echo " Creating Keptn project (hello-world) and a Keptn service (hello-world-service)            "
 echo "================================================================================="
-keptn create project hello-world --shipyard ./keptn/shipyard.yaml --git-remote-url $GIT_REPO --git-user $GIT_USER --git-token $GITHUB_TOKEN
+keptn create project hello-world --shipyard ./keptn/shipyard.yaml --git-remote-url $AZDO_REPO_URL --git-user $AZDO_USER --git-token $AZDO_TOKEN
 keptn create service hello-world-service --project=hello-world
+
+echo ""
+echo "================================================================================="
+echo "Adding Helm Chart (hello-world.tgz) to Git REPO"
+echo "================================================================================="
+keptn add-resource --project=hello-world --service=hello-world-service --all-stages --resource=./helm/hello-world-0.1.0.tgz --resourceUri=charts/hello-world-0.1.0.tgz
+
+
+echo ""
+echo "================================================================================="
+echo "Triggering Delivery of the helloservice v0.1.1 Artifact (Follow progress in the Bridge)"
+echo "================================================================================="
+keptn trigger delivery \
+--project=hello-world \
+--service=hello-world-service \
+--image="ghcr.io/podtato-head/podtatoserver:v0.1.1" \
+--labels=image="ghcr.io/podtato-head/podtatoserver",version="v0.1.1"
