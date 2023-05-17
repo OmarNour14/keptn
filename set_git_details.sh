4de41954e3be07bdf995dbf40f7fc16b433bbff8
Omar <omar.din@youlend.com>
2023-05-17 13:51:55 +0100

    fix: master: fix git file

--- set_git_details.sh
+++ set_git_details.sh
@@ -9,7 +9,7 @@ read -p 'Azure DevOps Repo name: ' AZDO_REPO_NAME
 
 export AZDO_USER=$AZDO_USER
 export AZDO_REPO_NAME=$AZDO_REPO_NAME
-export AZDO_REPO_URL= "https://youlend@dev.azure.com/youlend/Youlend-Infrastructure/_git/$AZDO_REPO_NAME"
+export AZDO_REPO_URL="https://youlend@dev.azure.com/youlend/Youlend-Infrastructure/_git/$AZDO_REPO_NAME"
 export AZDO_TOKEN=$AZDO_TOKEN
 
 echo ""
