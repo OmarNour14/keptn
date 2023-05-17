88e1eab3303610f4a448af3c5b1437101075297f
Omar <omar.din@youlend.com>
2023-05-17 15:14:56 +0100

    feat: master: add app and dockerfile

--- setup_keptn.sh
+++ setup_keptn.sh
@@ -7,19 +7,20 @@ echo "==========================================================================
 keptn create project hello-world --shipyard ./keptn/shipyard.yaml --git-remote-url $AZDO_REPO_URL --git-user $AZDO_USER --git-token $AZDO_TOKEN
 keptn create service hello-world-service --project=hello-world
 
-echo ""
-echo "================================================================================="
-echo "Adding Helm Chart (hello-world.tgz) to Git REPO"
-echo "================================================================================="
-keptn add-resource --project=hello-world --service=hello-world-service --all-stages --resource=./helm/hello-world-0.1.0.tgz --resourceUri=charts/hello-world-0.1.0.tgz
+# echo ""
+# echo "================================================================================="
+# echo "Adding Helm Chart (hello-world.tgz) to Git REPO"
+# echo "================================================================================="
+# keptn add-resource --project=hello-world --service=hello-world-service --all-stages --resource=./helm/hello-world-0.1.0.tgz --resourceUri=charts/hello-world-0.1.0.tgz
 
 
-echo ""
-echo "================================================================================="
-echo "Triggering Delivery of the helloservice v0.1.1 Artifact (Follow progress in the Bridge)"
-echo "================================================================================="
-keptn trigger delivery \
---project=hello-world \
---service=hello-world-service \
---image="ghcr.io/podtato-head/podtatoserver:v0.1.1" \
---labels=image="ghcr.io/podtato-head/podtatoserver",version="v0.1.1"
+# echo ""
+# echo "================================================================================="
+# echo "Triggering Delivery of the helloservice v0.1.1 Artifact (Follow progress in the Bridge)"
+# echo "================================================================================="
+# keptn trigger delivery \
+# --project=hello-world \
+# --service=hello-world-service \
+# --sequence=delivery \
+# --image="hello-world:0.1.0" \
+# --labels=version="v0.1.1"
